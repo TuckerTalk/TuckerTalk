726c2870e49437cf0b0000cdaa4c8412cccb12b5
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-09-13 11:56:18 -0400

    Update tuckerconf.php

--- tuckerconf.php
+++ tuckerconf.php
@@ -2,4 +2,19 @@
 
 // Main Configuration file for TuckerTalk
 
-define("SERVER_NAME", "http://example.com");
+define("server_name", "http://chat.example.com"); // Website you will be using (ie. chat.example.com). Does not need to use HTTPS, but it should.
+
+define("pusher_app_id", ""); // ID of your pusher app
+
+define("pusher_key", ""); // Key of your pusher app
+
+define("pusher_secret", ""); // Secret of your pusher app
+
+define("pusher_cluster", ""); // Cluster/Region of your pusher app
+
+// DO NOT EDIT UNDER THIS LINE, UNLESS YOU KNOW WHAT YOU ARE DOING...
+
+$ver = phpversion();
+if($ver =< 7){
+  die("Please use PHP 7 or higher to run TuckerTalk.");
+}
